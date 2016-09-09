739b2c82bc40abcaee1b7879ff7422187c2e0abe
syedkaleem29 <kaleem105@gmail.com>
2016-09-09 17:35:32 +0530

    changing the domain

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var main=function(){
+	$("#joinButton").click(function(){
+		$('.intro').hide();
+		$('.main').show();
+	});
+}
+$(document).ready(main);
--- mysite-final/js/main.js
+++
@@ -1,7 +0,0 @@
-var main=function(){
-	$("#joinButton").click(function(){
-		$('.intro').hide();
-		$('.main').show();
-	});
-}
-$(document).ready(main);
